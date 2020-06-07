0e8a4a26664de839ceb58e75758520d2e8a29cf1
Pokaboo <1152401192@qq.com>
2020-06-07 16:06:35 +0800

    [A] 头像上传功能

--- online_edu_parent/service/edu_service/src/main/java/com/pokaboo/eduservice/controller/EduVideoController.java
+++ online_edu_parent/service/edu_service/src/main/java/com/pokaboo/eduservice/controller/EduVideoController.java
@@ -1,9 +1,15 @@
 package com.pokaboo.eduservice.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import com.pokaboo.commonutils.Result;
+import com.pokaboo.eduservice.entity.EduVideo;
+import com.pokaboo.eduservice.service.EduVideoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -13,9 +19,66 @@ import org.springframework.web.bind.annotation.RestController;
  * @author pookaboo
  * @since 2020-05-31
  */
+@Api(value = "课时管理", tags = {"课时管理"})
 @RestController
-@RequestMapping("/eduservice/edu-video")
+@RequestMapping("/eduservice/video")
+@CrossOrigin
 public class EduVideoController {
 
+    @Autowired
+    private EduVideoService eduVideoService;
+
+
+    @ApiOperation(value = "添加课时")
+    @PostMapping("/saveVideo")
+    public Result saveEduVideo(
+            @ApiParam(value = "eduVideo", name = "课时信息", required = true)
+            @RequestBody EduVideo eduVideo
+    ) {
+        boolean save = eduVideoService.save(eduVideo);
+        if (save) {
+            return Result.ok();
+        }
+        return Result.error();
+    }
+
+    @ApiOperation(value = "修改课时")
+    @PostMapping("/updateEduVideo")
+    public Result updateEduVideo(
+            @ApiParam(value = "eduVideo", name = "课时信息", required = true)
+            @RequestBody EduVideo eduVideo
+    ) {
+        boolean update = eduVideoService.updateById(eduVideo);
+        if (update) {
+            return Result.ok();
+        }
+        return Result.error();
+    }
+
+    @ApiOperation(value = "删除课时")
+    @DeleteMapping("/{id}")
+    public Result deleteEduVideo(
+            @ApiParam(value = "id", name = "课时id", required = true)
+            @PathVariable String id
+    ) {
+        boolean remove = eduVideoService.removeById(id);
+        if (remove) {
+            return Result.ok();
+        }
+        return Result.error();
+    }
+
+    @ApiOperation(value = "根据id获取课时信息")
+    @GetMapping("/findEduVideo/{id}")
+    public Result findEduVideo(
+            @ApiParam(value = "", name = "", required = true)
+            @PathVariable String id
+    ) {
+        EduVideo eduVideo = eduVideoService.getById(id);
+        if (eduVideo != null) {
+            return Result.ok().data("video", eduVideo);
+        }
+        return Result.error();
+    }
 }
 
