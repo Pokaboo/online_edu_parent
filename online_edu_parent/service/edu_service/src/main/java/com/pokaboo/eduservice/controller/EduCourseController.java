08693b29a2dd72f760a989d9d3e6fcf1b9fad79d
Pokaboo <1152401192@qq.com>
2020-06-20 21:25:49 +0800

    【A】整合阿里云视频点播

--- online_edu_parent/service/edu_service/src/main/java/com/pokaboo/eduservice/controller/EduCourseController.java
+++ online_edu_parent/service/edu_service/src/main/java/com/pokaboo/eduservice/controller/EduCourseController.java
@@ -13,7 +13,6 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.xmlbeans.impl.xb.xsdschema.Public;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
